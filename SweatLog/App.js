ff589115c6b1a0544780780d5e86f6692a4b9387
Vaishnavi Kale <vaishnavi.kale3011@gmail.com>
2024-04-05 22:16:46 +0530

    native react tailwind added

--- SweatLog/App.js
+++ SweatLog/App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import exercises from './assets/data/exercises.json';
+import { StyleSheet } from 'react-native';
+import { styled } from 'nativewind';
+
+
+const StyledView = styled(View);
+const StyledText = styled(Text);
 
 export default function App() {
+  const exercise = exercises[0];
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <StyledView className='items-center justify-center flex-1'>
+      <StyledText className='text-slate-800'>{exercise.name}</StyledText>
+      <StyledText>Muscle: {exercise.muscle} | Equipment: {exercise.equipment}</StyledText>
       <StatusBar style="auto" />
-    </View>
+    </StyledView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
