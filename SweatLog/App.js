0db7c3e5b88124f66575b58770323d2b3bd2c021
Vaishnavi Kale <vaishnavi.kale3011@gmail.com>
2024-04-05 23:45:43 +0530

    Update App.js
    Co-Authored-By: Sapna Kirad <89734847+SapnaKirad03@users.noreply.github.com>
    Co-Authored-By: Apurva Patil <105532974+av2patil@users.noreply.github.com>

--- SweatLog/App.js
+++ SweatLog/App.js
@@ -1,18 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import exercises from './assets/data/exercises.json';
 
 
 export default function App() {
-  const exercise = exercises[0];
+  const exercise = exercises[4];
 
   return (
     <View style={styles.container}>
-      <View style={styles.exerciseContainer}>
+      <FlatList
+        data={exercises}
+        renderItem={({ item, index }) => {
+          return (
+            <View style={styles.exerciseContainer}>
+              <Text style={{ fontSize: 20, fontWeight: '700' }}>{index}. {item.name}</Text>
+              <Text style={{ color: 'dimgray' }}>
+                Muscle: {item.muscle.toUpperCase()} |
+                Equipment: {item.equipment.toUpperCase()}
+              </Text>
+            </View>
+          )
+        }}
+      />
 
-        <Text style={{ fontSize: 20, fontWeight: '700' }}>{exercise.name}</Text>
-        <Text style={{ color: 'dimgray' }}>Muscle: {exercise.muscle.toUpperCase()} | Equipment: {exercise.equipment}</Text>
-      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -29,6 +39,8 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     padding: 10,
     borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 3, height: 6 },
     gap: 5,
   }
 })
