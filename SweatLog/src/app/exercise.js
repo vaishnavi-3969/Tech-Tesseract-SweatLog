3a40243c63062e3d515252028fb9f104b47daea9
Vaishnavi Kale <vaishnavi.kale3011@gmail.com>
2024-04-12 00:22:01 +0530

    pressable

---
+++ SweatLog/src/app/exercise.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ExerciseDetailsScreen() {
+  return (
+    <View>
+      <Text>Exercise Details</Text>
+    </View>
+  )
+}
--- SweatLog/src/components/ExerciseListItem.jsx
+++ SweatLog/src/components/ExerciseListItem.jsx
@@ -1,16 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import styles from "../../styles";
+import { Link } from 'expo-router';
 
-function ExerciseListItem({ item }) {
+export default function ExerciseListItem({ item }) {
     return (
-        <View style={styles.exerciseContainer}>
-            <Text style={styles.exerciseName}>{item.name}</Text>
-            <Text style={styles.exerciseDetails}>
-                <Text style={styles.subValue}>{item.muscle} | {' '}</Text>
-                <Text style={styles.subValue}>{item.equipment}</Text>
-            </Text>
-        </View>
+        <Link href={'/exercise'} asChild>
+            <Pressable style={styles.exerciseContainer}>
+                <Text style={styles.exerciseName}>{item.name}</Text>
+                <Text style={styles.exerciseDetails}>
+                    <Text style={styles.subValue}>{item.muscle} | {' '}</Text>
+                    <Text style={styles.subValue}>{item.equipment}</Text>
+                </Text>
+            </Pressable>
+        </Link>
+
     )
 }
 
-export default ExerciseListItem;
